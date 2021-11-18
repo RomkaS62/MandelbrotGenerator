9d1ae3b391819296ca270eec842f0436c0390a87
Ramūnas Mažeikis <r.mazeikis@gmc.lt>
2021-11-18 10:40:16 +0200

    AND ANOTHER ONE.
    *Sigh*
    
    Signed-off-by: Ramūnas Mažeikis <r.mazeikis@gmc.lt>

---
+++ mbthreading.h
@@ -0,0 +1,14 @@
+#ifndef MANDELBROT_THREADING_H
+#define MANDELBROT_THREADING_H
+
+struct draw_lines_data {
+	struct bmp_img *img;
+	uint16_t ln_from;
+	uint16_t ln_to;
+	double from_x;
+	double from_y;
+	double step;
+};
+
+
+#endif /* MANDELBROT_THREADING_H */
