efb711800228091a68c9a44a1f0f91cfda90133d
Ramūnas Mažeikis <r.mazeikis@gmc.lt>
2021-11-18 10:38:48 +0200

    And another one.
    Signed-off-by: Ramūnas Mažeikis <r.mazeikis@gmc.lt>

---
+++ iterate.h
@@ -0,0 +1,14 @@
+#ifndef MANDELBROT_ITERATE_H
+#define MANDELBROT_ITERATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void * draw_lines(void *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MANDELBROT_ITERATE_H */
